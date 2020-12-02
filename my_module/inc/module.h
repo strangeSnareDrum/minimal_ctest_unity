4d32d15bd953dffedbbe5040f37a5eff85112e7d
Christian Hanner <christian.hanner@gmail.com>
2020-12-02 22:21:47 +0100

    Initial commit

---
+++ my_module/inc/module.h
@@ -0,0 +1,7 @@
+#ifndef MODULE_H
+#define MODULE_h
+
+int module_add(int a, int b);
+int module_sub(int a, int b);
+
+#endif // MODULE_H
---
+++ my_module/module.c
@@ -0,0 +1,9 @@
+#include <module.h>
+
+int module_add(int a, int b) {
+    return a + b;
+}
+
+int module_sub(int a, int b) {
+    return a - b;
+}
