53dfa6aec552f9eab1c3f6de4fd2d94c5668fa4a
Christian Hanner <christian.hanner@gmail.com>
2020-12-04 17:36:36 +0100

    Add the unity test framework

--- my_module/test/test1.c
+++ my_module/test/test1.c
@@ -1,3 +1,28 @@
-int main() {
-    return 0;
+#include <unity.h>
+#include <module.h>
+
+void setUp(void) {
+    // set stuff up here
+}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+void test_module_add(void) {
+    int a = 14, b = 8;
+    TEST_ASSERT_EQUAL_INT(a + b, module_add(a, b));
+}
+
+void test_module_sub(void) {
+    int a = 14, b = 8;
+    TEST_ASSERT_EQUAL_INT(a - b, module_sub(a, b));
+}
+
+// not needed when using generate_test_runner.rb
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_module_add);
+    RUN_TEST(test_module_sub);
+    return UNITY_END();
 }
